1bf059423f007e337b355babd1f7c9c94cdf7acb
Gustavo Maldonado <gamcba7982@gmail.com>
2022-12-19 15:42:56 -0300

    no action bar added

--- app/src/main/java/com/moviemain/ui/MainActivity.kt
+++ app/src/main/java/com/moviemain/ui/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.moviemain.ui
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.moviemain.R
 import com.moviemain.databinding.ActivityMainBinding
