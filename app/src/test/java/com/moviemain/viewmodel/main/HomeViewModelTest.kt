7c33262d2fd92c662106d171525d48056a10aa70
Gustavo Maldonado <gamcba7982@gmail.com>
2023-01-09 14:21:02 -0300

    first test with retrofit in homeviewmodel

--- app/src/test/java/com/moviemain/viewmodel/main/HomeViewModelTest.kt
+++ app/src/test/java/com/moviemain/viewmodel/main/HomeViewModelTest.kt
@@ -2,21 +2,31 @@ package com.moviemain.viewmodel.main
 
 import com.moviemain.core.common.Constants
 import com.moviemain.domain.RepositoryImpl
+import com.moviemain.model.data.Movie
+import com.moviemain.model.local.LocalDataSource
+import com.moviemain.model.local.MovieDao
 import com.moviemain.model.remote.ApiService
+import com.moviemain.model.remote.RemoteDataSource
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.notNullValue
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.BeforeClass
+import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeViewModelTest {
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var apiService: ApiService
-    private lateinit var repository: RepositoryImpl
 
     companion object {
+        const val API_KEY = "5ab6b649a24299a96dc96faa2c825afa"
+        const val LANGUAGE_es_ES = "es-ES"
         private lateinit var retrofit: Retrofit
 
         @BeforeClass
@@ -38,7 +48,47 @@ class HomeViewModelTest {
 
     @Before
     fun setup() {
-        homeViewModel = HomeViewModel(repository)
         apiService = retrofit.create(ApiService::class.java)
     }
+
+    @Test
+    fun checkFetchMainMoviesPopularIsNotNullTest() {
+        runBlocking {
+            val result = apiService.getPopularMovies(API_KEY, LANGUAGE_es_ES)
+            assertThat(result.results, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun checkFetchMainMoviesTopRatedIsNotNullTest() {
+        runBlocking {
+            val result = apiService.getTopRatedMovies(API_KEY, LANGUAGE_es_ES)
+            assertThat(result.results, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun checkFetchMainMoviesNowPlayingIsNotNullTest() {
+        runBlocking {
+            val result = apiService.getNowPlayingMovies(API_KEY, LANGUAGE_es_ES)
+            assertThat(result.results, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun checkFetchMainMovieUpcomingIsNotNullTest() {
+        runBlocking {
+            val result = apiService.getNowPlayingMovies(API_KEY, LANGUAGE_es_ES)
+            assertThat(result.results, `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun checkItemMoviesForPageTest() {
+        runBlocking {
+            val result = apiService.getPopularMovies(API_KEY, LANGUAGE_es_ES)
+            assertThat(result.results.size, `is`(20))
+        }
+    }
+
 }
