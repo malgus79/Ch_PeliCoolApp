c9f4e1e5ca8b6a9907c5056442b756ba575260d0
Gustavo Maldonado <gamcba7982@gmail.com>
2023-01-10 12:33:47 -0300

    test room database

--- app/src/androidTest/java/com/moviemain/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/moviemain/ExampleInstrumentedTest.kt
@@ -16,7 +16,7 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
+    fun testUseAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.moviemain", appContext.packageName)
---
+++ app/src/androidTest/java/com/moviemain/model/local/MovieDatabaseTest.kt
@@ -0,0 +1,89 @@
+package com.moviemain.model.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class MovieDatabaseTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: MovieDatabase
+
+    @Before
+    fun setUp() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            MovieDatabase::class.java
+        )
+            .allowMainThreadQueries().build()
+
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testIsDatabaseNotOpen() {
+        assertThat(database.isOpen).isFalse()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testIsDatabaseOpen() = runTest {
+        executeDatabaseFunction()
+        assertThat(database.isOpen).isTrue()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testDatabaseVersionIsCurrent() = runTest {
+        executeDatabaseFunction()
+        assertThat(database.openHelper.readableDatabase.version).isEqualTo(2)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testDatabasePathIsMemory() = runTest {
+        executeDatabaseFunction()
+        assertThat(database.openHelper.readableDatabase.path).isEqualTo(":memory:")
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    @ExperimentalCoroutinesApi
+    private fun executeDatabaseFunction() = runTest {
+        val movieEntity = MovieEntity(
+            76600,
+            false,
+            "/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+            "en",
+            "Avatar: The Way of Water",
+            "Ambientada más de una década después de los acontecimientos de la primera película, 'Avatar: The Way of Water' empieza contando la historia de la familia Sully (Jake, Neytiri y sus hijos), los problemas que los persiguen, lo que tienen que hacer para mantenerse a salvo, las batallas que libran para seguir con vida y las tragedias que sufren.",
+            4762.653,
+            "/kUAG4ZQcsNbRyiPyAr3hLdsVgAq.jpg",
+            "2022-12-14",
+            "Avatar: El sentido del agua",
+            false,
+            7.7,
+            3902
+        )
+        database.movieDao().saveMovie(movieEntity)
+    }
+
+}
